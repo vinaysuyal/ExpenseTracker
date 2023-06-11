525043a77ab4a3f5e3b87c9249753e916368e723
vinaysuyal <vinaysuyal2012@gmail.com>
2023-06-11 13:39:23 +0530

    Form made responsive

--- src/Components/AddEditCard.jsx
+++ src/Components/AddEditCard.jsx
@@ -22,8 +22,8 @@ const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
+  width: "80vw",
   transform: "translate(-50%, -50%)",
-  width: 400,
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
@@ -191,7 +191,7 @@ export default function AddEditCard({
             variant="contained"
             color="primary"
           >
-            Submit
+            Add Expense
           </Button>
         </Box>
       </Modal>
